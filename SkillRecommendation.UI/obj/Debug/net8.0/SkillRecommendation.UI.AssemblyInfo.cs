ce02ed973a7b635ba23adf068fbb44970688b18b
aaronvancuren <99290929+aaronvancuren@users.noreply.github.com>
2024-04-12 14:53:32 -0400

    updated the styling and fixed form submission

--- SkillRecommendation.UI/obj/Debug/net8.0/SkillRecommendation.UI.AssemblyInfo.cs
+++ SkillRecommendation.UI/obj/Debug/net8.0/SkillRecommendation.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkillRecommendation.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d95ca18e86e5480a76c8c01566573a9ee71e1b6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c41b27a1f2bab94f2c84a3852a99059adce778a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkillRecommendation.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkillRecommendation.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
